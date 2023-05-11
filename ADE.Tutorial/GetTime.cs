06eae1ccc402fc4333422207c0595ca9763ead45
David Triana <david@davidtriana.com>
2023-05-11 09:46:24 -0500

    Update GetTime.cs

--- ADE.Tutorial/GetTime.cs
+++ ADE.Tutorial/GetTime.cs
@@ -34,7 +34,7 @@ namespace ADE.Tutorial
 
             var now = DateTime.UtcNow;
 
-            response.WriteString($"UTC Time: {now}");
+            response.WriteString($"UTC Time is: {now}\n");
 
             foreach (var timeZone in timeZones)
                 response.WriteString($"{(timeZone.IsDaylightSavingTime(now) ? timeZone.DaylightName : timeZone.StandardName)}: {TimeZoneInfo.ConvertTimeFromUtc(now, timeZone)}");
